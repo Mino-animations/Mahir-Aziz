0cecbd08b4617f8d0673b3dff142aade1ab59f0a
Mino-animations <minokaki162@gmail.com>
2025-02-09 13:25:53 -0500

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,14 +6,18 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Mahir",
+  firstName: "Hashir",
   middleName: "",
-  lastName: "Aziz",
-  message: " Passionate about making art alive through animation. ",
+  lastName: "Shoaib",
+  message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
       url: "https://github.com/hashirshoaeb",
+    },
+    {
+      image: "fa-facebook",
+      url: "https://www.facebook.com/hashirshoaeb",
     },
     {
       image: "fa-instagram",
@@ -50,7 +54,7 @@ const about = {
   imageLink: require("../editable-stuff/hashirshoaeb.png"),
   imageSize: 375,
   message:
-    "My name is Mahir Aziz. I’m a student of George Washington University with an expectation to graduate in 2028. I have a great passion for 2D animation and action. ",
+    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
   resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 };
 
@@ -116,6 +120,7 @@ const skills = {
     { name: "Creativity", value: 90 },
   ],
 };
+
 // GET IN TOUCH SECTION
 const getInTouch = {
   show: true,
